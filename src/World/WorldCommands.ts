b8ffb0842197d161ae9649eae95133116582f367
JaimeGensler <jaimegensler0@gmail.com>
2022-10-02 21:16:46 -0700

    Rough rework to WorldCommands

--- src/World/WorldCommands.ts
+++ src/World/WorldCommands.ts
@@ -1,65 +1,36 @@
-import BigUintArray from '../utils/DataTypes/BigUintArray';
-import IndexAllocator from '../utils/DataTypes/IndexAllocator';
-import SparseSet from '../utils/DataTypes/SparseSet';
-import { ThreadProtocol } from '../utils/Thread';
+import type Entities from './Entities';
 import type { ComponentType } from '../Components';
-import type { WorldConfig } from './config';
 
 export default class WorldCommands {
-	static fromWorld(config: WorldConfig, componentCount: number) {
-		return new this(
-			IndexAllocator.with(config.maxEntities, config.threads > 1),
-			BigUintArray.with(
-				componentCount,
-				config.maxEntities,
-				config.threads > 1,
-			),
-			SparseSet.with(config.maxEntities, config.threads > 1),
-		);
-	}
+	queue = new Map<bigint, bigint>(); // Map<eid, tableid>
 
+	#entities: Entities;
 	#entityCommands: InternalEntityCommands;
-
-	#allocator: IndexAllocator;
-	entityData: BigUintArray;
-	modifiedEntities: SparseSet;
-	constructor(
-		allocator: IndexAllocator,
-		entityData: BigUintArray,
-		modifiedEntities: SparseSet,
-	) {
-		this.#allocator = allocator;
-		this.entityData = entityData;
+	constructor(entities: Entities, components: Set<ComponentType>) {
+		this.#entities = entities;
 		this.#entityCommands = new EntityCommands(
 			this,
-			entityData,
-			modifiedEntities,
+			entities,
+			components,
+			this.queue,
 		) as any;
-		this.modifiedEntities = modifiedEntities;
 	}
 
-	private __$$setComponents(components: Map<ComponentType, object>) {
-		this.#entityCommands.__$$setComponents(components);
-	}
-
-	// === Entities ===
 	/**
 	 * Queues an entity to be spawned.
 	 * @returns `EntityCommands` to add/remove components from an entity.
 	 */
 	spawn(): EntityCommands {
-		return this.#entityCommands.__$$setId(this.#allocator.get());
+		return this.#entityCommands.__$$setId(this.#entities.spawn());
 	}
 
 	/**
 	 * Queues an entity to be despawned.
-	 * @param uuid The UUID (Entity & Generation ID) of the entity to despawn.
-	 * @returns `this`
+	 * @param id The id of the entity to despawn.
+	 * @returns `this`, for chaining.
 	 */
-	despawn(id: number): this {
-		this.#allocator.free(id);
-		this.entityData.set(id, 0n);
-		this.modifiedEntities.add(id);
+	despawn(id: bigint): this {
+		this.queue.set(id, 0n);
 		return this;
 	}
 
@@ -68,88 +39,75 @@ export default class WorldCommands {
 	 * @param id The id of the entity to get.
 	 * @returns `EntityCommands` to add/remove components from an entity.
 	 */
-	get(id: number): EntityCommands {
+	get(id: bigint): EntityCommands {
 		return this.#entityCommands.__$$setId(id);
 	}
-
-	[ThreadProtocol.Send](): SerializedWorldCommands {
-		return [this.#allocator, this.entityData, this.modifiedEntities];
-	}
-	static [ThreadProtocol.Receive]([
-		alloc,
-		entityData,
-		modified,
-	]: SerializedWorldCommands) {
-		return new this(alloc, entityData, modified);
-	}
 }
-type SerializedWorldCommands = [IndexAllocator, BigUintArray, SparseSet];
 
 type InternalEntityCommands = {
 	[Key in keyof EntityCommands]: EntityCommands[Key];
 } & {
-	__$$setComponents(components: Map<ComponentType, object>): void;
-	__$$setId(id: number): EntityCommands;
+	__$$setId(id: bigint): EntityCommands;
 };
 
 class EntityCommands {
-	private __$$setId(id: number): this {
+	private __$$setId(id: bigint): this {
 		this.#id = id;
 		return this;
 	}
-	private __$$setComponents(components: Map<ComponentType, object>) {
-		this.#components = components;
-	}
-
-	#id = 0;
-	#components: Map<ComponentType, object>;
+	#id = 0n;
 
 	#worldCommands: WorldCommands;
-	#entityData: BigUintArray;
-	#modifiedEntities: SparseSet;
+	#entities: Entities;
+	#components: Set<ComponentType>;
+	#queue: Map<bigint, bigint>;
 	constructor(
 		worldCommands: WorldCommands,
-		entityData: BigUintArray,
-		modifiedEntities: SparseSet,
+		entities: Entities,
+		components: Set<ComponentType>,
+		queue: Map<bigint, bigint>,
 	) {
 		this.#worldCommands = worldCommands;
-		this.#components = null!;
-		this.#entityData = entityData;
-		this.#modifiedEntities = modifiedEntities;
+		this.#entities = entities;
+		this.#components = components;
+		this.#queue = queue;
 	}
 
-	initialize(Component: ComponentType<any, any>): this {
-		const componentId = getMapIndex(this.#components, Component);
-		const entityComponents = this.#entityData.get(this.#id);
-		if ((entityComponents & (1n << BigInt(componentId))) === 0n) {
-			this.insert(Component);
-		}
-		return this;
-	}
-
-	insert(Component: ComponentType<any, any>): this {
-		this.#entityData.OR(
+	/**
+	 * Queues a component to be inserted into this entity.
+	 * @param Component The Component **class** to insert into the entity.
+	 * @returns `this`, for chaining.
+	 */
+	insert(Component: ComponentType<any>): this {
+		this.#queue.set(
 			this.#id,
-			1n << BigInt(getMapIndex(this.#components, Component)),
+			(this.#queue.get(this.#id) ?? this.#entities.getTableId(this.#id)) &
+				(1n << BigInt(getSetIndex(this.#components, Component))),
 		);
-		this.#modifiedEntities.add(this.#id);
 		return this;
 	}
 
+	/**
+	 * Queues a component to be removed from this entity.
+	 * @param Component The Component **class** to remove from the entity.
+	 * @returns `this`, for chaining.
+	 */
 	remove(Component: ComponentType): this {
-		this.#entityData.XOR(
+		this.#queue.set(
 			this.#id,
-			1n << BigInt(getMapIndex(this.#components, Component)),
+			(this.#queue.get(this.#id) ?? this.#entities.getTableId(this.#id)) ^
+				(1n << BigInt(getSetIndex(this.#components, Component))),
 		);
-		this.#modifiedEntities.add(this.#id);
 		return this;
 	}
 
+	/**
+	 * Queues this entity to be despawned.
+	 * @returns `void`
+	 */
 	despawn(): void {
 		this.#worldCommands.despawn(this.#id);
 	}
 }
 
-function getMapIndex<K>(map: Map<K, any>, key: K): number {
-	return [...map.keys()].indexOf(key);
-}
+const getSetIndex = <T>(set: Set<T>, key: T) => [...set].indexOf(key);
