3e751b2d1d986957f4f45b5343379fa2fa47a4eb
JaimeGensler <jaimegensler0@gmail.com>
2022-10-21 22:55:52 -0700

    Expand World API to move entities

--- src/World/World.ts
+++ src/World/World.ts
@@ -1,5 +1,5 @@
 import WorldBuilder from './WorldBuilder';
-import Table from '../Components/Table';
+import { ComponentStore, ComponentType, Entity, Table } from '../Components';
 import validateAndCompleteConfig, {
 	type WorldConfig,
 	type SingleThreadedWorldConfig,
@@ -10,6 +10,15 @@ import type WorldCommands from './WorldCommands';
 import type Entities from './Entities';
 import type { System } from '../Systems';
 import type { ResourceType } from '../Resources';
+import bits from '../utils/bits';
+import zipIntoMap from '../utils/zipIntoMap';
+import { Query } from '../Queries';
+
+const NEW_TABLE = 'thyseus::newTable';
+type NewTablePayload = [bigint, ComponentStore[], Uint32Array];
+
+const GROW_TABLE = 'thyseus::growTable';
+type GrowTablePayload = [bigint, ComponentStore[]];
 
 export default class World {
 	static new(config?: Partial<SingleThreadedWorldConfig>): WorldBuilder;
@@ -21,15 +30,17 @@ export default class World {
 		return new WorldBuilder(validateAndCompleteConfig(config, url), url);
 	}
 
-	#archetypes = new Map<bigint, Table>();
+	archetypes = new Map<bigint, Table>();
+	queries = [] as Query<any>[];
 
-	#config: WorldConfig;
-	#resources: Map<ResourceType, object>;
-	#threads: ThreadGroup;
+	config: WorldConfig;
+	resources: Map<ResourceType, object>;
+	threads: ThreadGroup;
 	#systems: System[];
 	#executor: Executor;
-	#commands: WorldCommands;
-	#entities: Entities;
+	commands: WorldCommands;
+	entities: Entities;
+	components: ComponentType[];
 	constructor(
 		config: WorldConfig,
 		resources: Map<ResourceType, object>,
@@ -38,44 +49,111 @@ export default class World {
 		executor: Executor,
 		commands: WorldCommands,
 		entities: Entities,
+		components: ComponentType[],
 	) {
-		this.#config = config;
-		this.#resources = resources;
-		this.#threads = threads;
+		this.config = config;
+		this.resources = resources;
+		this.threads = threads;
 		this.#systems = systems;
 		this.#executor = executor;
-		this.#commands = commands;
-		this.#entities = entities;
+		this.commands = commands;
+		this.entities = entities;
+		this.components = components;
+
+		this.threads.setListener<NewTablePayload>(
+			NEW_TABLE,
+			([tableId, stores, meta]) => {
+				const columns = zipIntoMap(
+					[...bits(tableId)].map(cid => this.components[cid]),
+					stores,
+				);
+				const table = new Table(columns, meta);
+				this.archetypes.set(tableId, table);
+				for (const query of this.queries) {
+					//@ts-ignore
+					query.testAdd(tableId, table);
+				}
+			},
+		);
+		this.threads.setListener<GrowTablePayload>(
+			GROW_TABLE,
+			([tableId, stores]) => {
+				const table = this.archetypes.get(tableId)!;
+				let i = 0;
+				for (const key of table.columns.keys()) {
+					table.columns.set(key, stores[i++]);
+				}
+			},
+		);
 
 		this.#executor.onReady(() => this.#runSystems());
 	}
 
-	get config() {
-		return this.#config;
+	moveEntity(entityId: bigint, targetTableId: bigint) {
+		const currentTableId = this.entities.getTableId(entityId);
+		const currentTable = this.archetypes.get(currentTableId);
+		if (targetTableId === 0n) {
+			this.#deleteEntity(entityId);
+			return;
+		}
+
+		const targetTable = this.#getTable(targetTableId);
+		if (targetTable.isFull) {
+			this.#resizeTable(targetTableId, targetTable);
+		}
+
+		if (currentTable) {
+			this.entities.setLocation(
+				currentTable.columns.get(Entity)![currentTable.size - 1],
+				currentTableId,
+				this.entities.getRow(entityId),
+			);
+			currentTable.move(this.entities.getRow(entityId), targetTable);
+		} else {
+			targetTable.add(entityId);
+		}
+		this.entities.setLocation(entityId, targetTableId, targetTable.size);
 	}
-	get threads() {
-		return this.#threads;
-	}
-	get resources() {
-		return this.#resources;
-	}
-	get archetypes() {
-		return this.#archetypes;
-	}
-	get commands() {
-		return this.#commands;
+	#deleteEntity(entityId: bigint) {
+		const tableId = this.entities.getTableId(entityId);
+		const table = this.archetypes.get(tableId);
+		const entityRow = this.entities.getRow(entityId);
+		this.entities.setLocation(
+			table?.columns.get(Entity)?.val?.[0],
+			tableId,
+			entityRow,
+		);
+		table?.delete(this.entities.getRow(entityId));
+		this.entities.setLocation(entityId, 0n, 0);
 	}
 
-	moveEntity(entityId: bigint, tableId: bigint) {}
-
-	// getTable(tableId: bigint): Table {
-	// 	if (!this.#archetypes.has(tableId)) {
-	// 		const table = Table.fromWorld();
-	// 		this.#archetypes.set(tableId, table);
-	// 	}
-	// 	return this.#archetypes.get(tableId)!;
-	// }
-	// resizeTable(table: Table): void {}
+	#getTable(tableId: bigint): Table {
+		if (!this.archetypes.has(tableId)) {
+			const table = Table.create(
+				[...bits(tableId)].map(cid => this.components[cid]),
+				this.config.getNewTableSize(0),
+			);
+			this.threads.send<NewTablePayload>(NEW_TABLE, [
+				tableId,
+				[...table.columns.values()],
+				table.meta,
+			]);
+			this.archetypes.set(tableId, table);
+			for (const query of this.queries) {
+				//@ts-ignore
+				query.testAdd(tableId, table);
+			}
+		}
+		return this.archetypes.get(tableId)!;
+	}
+	#resizeTable(tableId: bigint, table: Table) {
+		table.grow(this.config);
+		this.threads.send<GrowTablePayload>(GROW_TABLE, [
+			tableId,
+			[...table.columns.values()],
+			table.meta,
+		]);
+	}
 
 	async update() {
 		this.#executor.reset();
