42bdbfc2879c2a48fa3d94d28b7a0242d42ee350
JaimeGensler <jaimegensler0@gmail.com>
2022-10-21 22:53:04 -0700

    Add Resource export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default, definePlugin } from './World';
 export { defineSystem, P, Mut, applyCommands } from './Systems';
+export { Resource } from './Resources';
 export { Component, Type, Entity } from './Components';
 export { ThreadProtocol } from './utils/Threads';
