46e7a26fcf3cc28d330f6a8700e73b1bc62079eb
JaimeGensler <jaimegensler0@gmail.com>
2022-10-23 12:19:08 -0700

    Remove Resource export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { World, definePlugin } from './World';
 export { defineSystem, applyCommands } from './Systems';
-export { Resource } from './Resources';
 export { struct, Entity } from './Components';
