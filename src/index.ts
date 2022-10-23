fe4201d6e1b5e47c8074e817c5b7cc3b5dcf2ead
JaimeGensler <jaimegensler0@gmail.com>
2022-10-23 11:09:35 -0700

    Remove top-level ThreadProtocol export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { World, definePlugin } from './World';
 export { defineSystem, applyCommands } from './Systems';
 export { Resource } from './Resources';
 export { struct, Entity } from './Components';
-export { ThreadProtocol } from './utils/Threads';
