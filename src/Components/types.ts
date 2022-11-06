8d5521cec79d666a2b12a6941da2c842fde46529
JaimeGensler <jaimegensler0@gmail.com>
2022-11-06 08:59:58 -0800

    Begin expanding ComponentType definition

--- src/Components/types.ts
+++ src/Components/types.ts
@@ -11,7 +11,7 @@ export type TypedArrayConstructor =
 	| BigInt64ArrayConstructor
 	| Float32ArrayConstructor
 	| Float64ArrayConstructor;
-type TypedArray =
+export type TypedArray =
 	| Uint8Array
 	| Uint16Array
 	| Uint32Array
@@ -23,13 +23,30 @@ type TypedArray =
 	| Float32Array
 	| Float64Array;
 
-type Schema = Record<string | symbol, TypedArrayConstructor>;
 export type ComponentStore = Record<string | symbol, TypedArray>;
 
 export interface ComponentType {
 	// NOTE: Types have been loosened to be optional here, as decorators do not provide type info.
-	schema?: Schema;
+
+	/**
+	 * The schema used to create stores for this ComponentType.
+	 */
+	schema?: Record<string | symbol, TypedArrayConstructor>;
+	fieldSizes?: Record<string | symbol, number>;
+
+	/**
+	 * The size of this type, including padding. Always a multiple of alignment.
+	 */
 	size?: number;
+	/**
+	 * The raw size of this type, _not_ including padding.
+	 */
+	rawSize?: number;
+	/**
+	 * The alignment of this type - equal to the number of bytes of the largest primitive type this ComponentType contains.
+	 */
+	alignment?: number;
+
 	// TODO: Narrow store - it can be made more specific, but this makes Entity work for now.
 	new (store: any, index: number, commands: WorldCommands): object;
 }
