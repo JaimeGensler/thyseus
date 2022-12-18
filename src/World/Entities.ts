94b69f3270ec368d4de819308f520b67493da255
JaimeGensler <jaimegensler0@gmail.com>
2022-12-18 15:51:55 -0800

    Rework Entities for new Table storage

--- src/World/Entities.ts
+++ src/World/Entities.ts
@@ -1,171 +1,129 @@
-import { BigUintArray } from '../utils/BigUintArray';
+import { Entity, Table } from '../storage';
 import type { World } from './World';
 
-const nextId = 0;
-const freeCount = 1;
-
-const fourBytes = 32n;
+const NEXT_ID = 0;
+const CURSOR = 1;
 const lo32 = 0x00_00_00_00_ff_ff_ff_ffn;
 const getIndex = (id: bigint): number => Number(id & lo32);
-const getGeneration = (id: bigint): number => Number(id >> fourBytes);
 
-// NOTE: If tableIds move to uint32s, tableIds/row can be transformed into entityLocation: BigUint64Array
+const ENTITY_BATCH_SIZE = 256;
+
 export class Entities {
 	static fromWorld(world: World): Entities {
-		const size = world.config.maxEntities;
-		return new Entities(
-			world.threads.queue(
-				() => new Uint32Array(world.createBuffer(size * 4)),
-			),
-			new BigUintArray(
-				world.components.length,
-				size,
-				world.threads.queue(() => new Uint8Array(size)),
-			),
-			world.threads.queue(
-				() => new Uint32Array(world.createBuffer(size * 4)),
-			),
-			world.threads.queue(
-				() => new Uint32Array(world.createBuffer(2 * 4)),
-			),
+		return new this(
 			world.threads.queue(
 				() =>
 					new Uint32Array(
-						world.createBuffer(Math.ceil(size / 8) * 4),
+						world.createBuffer(
+							ENTITY_BATCH_SIZE * Uint32Array.BYTES_PER_ELEMENT,
+						),
 					),
 			),
+			world.threads.queue(() => new Uint32Array(world.createBuffer(8))),
+			world.archetypes[0],
 		);
 	}
 
-	generations: Uint32Array;
-	tableIds: BigUintArray;
-	row: Uint32Array;
-	#data: Uint32Array; // [NextId, FreeCount]
-	#free: Uint32Array;
-	constructor(
-		generations: Uint32Array,
-		tableIds: BigUintArray,
-		row: Uint32Array,
-		data: Uint32Array,
-		free: Uint32Array,
-	) {
-		this.generations = generations;
-		this.tableIds = tableIds;
-		this.row = row;
+	#locations: Uint32Array;
+	#data: Uint32Array; // [NEXT_ID, CURSOR]
+	#recycled: Table;
+	constructor(locations: Uint32Array, data: Uint32Array, recycled: Table) {
+		this.#locations = locations;
 		this.#data = data;
-		this.#free = free;
+		this.#recycled = recycled;
 	}
 
+	/**
+	 * A lockfree method to obtain a new Entity ID
+	 */
 	spawn(): bigint {
-		for (let i = 0; i < this.#free.length; i++) {
-			if (Atomics.load(this.#data, freeCount) === 0) {
-				break;
+		let currentCursor = Atomics.load(this.#data, CURSOR);
+		const recycledSize = this.#recycled.size;
+		while (currentCursor < recycledSize) {
+			const preExchangeValue = Atomics.compareExchange(
+				this.#data,
+				CURSOR,
+				currentCursor,
+				currentCursor + 1,
+			);
+			if (currentCursor === preExchangeValue) {
+				return this.#recycled.columns.get(Entity)!.u64![
+					recycledSize - currentCursor - 1
+				];
 			}
-			while (true) {
-				const n = Atomics.load(this.#free, i);
-				if (n === 0) {
-					break;
-				}
-				const result = ctz32(n);
-				if (Atomics.xor(this.#free, i, 0b1 << result) === n) {
-					Atomics.sub(this.#data, freeCount, 1);
-					return (
-						(BigInt(this.generations[result]) << fourBytes) |
-						BigInt(32 * i + result)
-					);
-				}
-			}
+			currentCursor = Atomics.load(this.#data, CURSOR);
 		}
-
-		const result = Atomics.add(this.#data, nextId, 1);
-		return BigInt(result);
+		return BigInt(Atomics.add(this.#data, NEXT_ID, 1));
 	}
 
-	despawn(id: bigint): void {
-		const index = getIndex(id);
-		const generation = getGeneration(id);
-		if (
-			Atomics.compareExchange(
-				this.generations,
-				index,
-				generation,
-				generation + 1,
-			) === generation
-		) {
-			Atomics.or(this.#free, index >> 5, 1 << (index & 0b0001_1111));
-			Atomics.add(this.#data, freeCount, 1);
-		}
+	resetCursor() {
+		this.#data[CURSOR] = 0;
 	}
 
-	getTableId(id: bigint): bigint {
-		return this.tableIds.get(getIndex(id));
-	}
-	getRow(id: bigint): number {
-		return this.row[getIndex(id)];
+	grow(world: World) {
+		// TODO
 	}
 
-	setLocation(entityId: bigint, tableId: bigint, row: number) {
-		this.tableIds.set(getIndex(entityId), tableId);
-		this.row[getIndex(entityId)] = row;
+	getTableIndex(entityId: bigint): number {
+		return this.#locations[getIndex(entityId) << 1] ?? 0;
+	}
+	setTableIndex(entityId: bigint, tableIndex: number) {
+		this.#locations[getIndex(entityId) << 1] = tableIndex;
+	}
+
+	getRow(entityId: bigint): number {
+		return this.#locations[(getIndex(entityId) << 1) + 1] ?? 0;
+	}
+	setRow(entityId: bigint, row: number) {
+		this.#locations[(getIndex(entityId) << 1) + 1] = row;
 	}
 }
-
-// NOTE: Requires n is non-zero
-const ctz32 = (n: number) => {
-	n >>>= 0;
-	return 31 - Math.clz32(n & -n);
-};
 
 /*---------*\
 |   TESTS   |
 \*---------*/
 if (import.meta.vitest) {
 	const { it, expect } = import.meta.vitest;
-
-	const createEntities = (length: number = 256) =>
-		new Entities(
-			new Uint32Array(length),
-			new BigUintArray(8, length, new Uint8Array(length)),
-			new Uint32Array(length),
-			new Uint32Array(3),
-			new Uint32Array(length),
-		);
-
-	it('ctz counts trailing zeroes', () => {
-		for (let i = 0; i < 32; i++) {
-			expect(ctz32(Number(`0b1${'0'.repeat(i)}`))).toBe(i);
-		}
-	});
+	const { Table } = await import('../storage');
 
 	it('returns incrementing generational integers', () => {
-		const entities = createEntities();
+		const entities = new Entities(
+			new Uint32Array(256 * 2),
+			new Uint32Array(3),
+			new Table(new Map(), 0, new Uint32Array(1), 0),
+		);
 
 		for (let i = 0n; i < 256n; i++) {
 			expect(entities.spawn()).toBe(i);
 		}
 	});
 
-	it('returns freed indices with incremented generations', () => {
-		const entities = createEntities(32);
+	it('returns entities from the Recycled table', () => {
+		const table = new Table(
+			new Map().set(Entity, { u64: new BigUint64Array(8) }),
+			0,
+			new Uint32Array(1),
+			0,
+		);
 
-		for (let i = 0; i < 32; i++) {
-			entities.spawn();
-		}
-		const frees = [1, 2, 3, 9, 10, 16, 24, 25];
-		frees.forEach(i => entities.despawn(BigInt(i)));
+		const entities = new Entities(
+			new Uint32Array(256 * 2),
+			new Uint32Array(3),
+			table,
+		);
 
-		for (const i of frees) {
-			const result = entities.spawn();
-			expect(getGeneration(result)).toBe(1);
-			expect(getIndex(result)).toBe(i);
-		}
+		expect(entities.spawn()).toBe(0n);
+		expect(entities.spawn()).toBe(1n);
+		expect(entities.spawn()).toBe(2n);
 
-		let prev = 0n;
-		for (let i = 1; i < 32; i++) {
-			entities.despawn(prev);
-			prev = entities.spawn();
-			expect(getIndex(prev)).toBe(0);
-			expect(getGeneration(prev)).toBe(i);
-		}
+		table.size = 3;
+		table.columns.get(Entity)!.u64![0] = 0n;
+		table.columns.get(Entity)!.u64![1] = 1n;
+		table.columns.get(Entity)!.u64![2] = 2n;
+
+		expect(entities.spawn()).toBe(2n);
+		expect(entities.spawn()).toBe(1n);
+		expect(entities.spawn()).toBe(0n);
+		expect(entities.spawn()).toBe(3n);
 	});
 }
