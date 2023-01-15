09f6b7ae6cf3df7c04630ee34b5556c2855274c3
JaimeGensler <jaimegensler0@gmail.com>
2023-01-15 12:29:01 -0800

    Add initStruct to consumer exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { World, definePlugin } from './world';
 export { defineSystem, applyCommands } from './systems';
 export { Entity } from './storage';
-export { struct } from './struct';
+export { struct, initStruct } from './struct';
 export { createThreadChannel } from './threads';
