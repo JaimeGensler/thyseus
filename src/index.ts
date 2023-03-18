f55ed2f8c713475cbba06ab86f274fd88e588609
JaimeGensler <jaimegensler0@gmail.com>
2023-03-18 09:22:04 -0700

    Expose memory

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export { defineSystem } from './systems';
 export { Entity, initStruct, dropStruct } from './storage';
 export { struct } from './struct';
 export { createThreadChannel } from './threads';
+export { memory } from './utils/memory';
 
 export type { WorldBuilder, Plugin, WorldConfig } from './world';
