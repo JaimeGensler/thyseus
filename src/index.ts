14f45b2196c43849aeb615ee14390a9a9b54b7bb
JaimeGensler <jaimegensler0@gmail.com>
2023-04-02 11:50:32 -0700

    Update exports to remove dropped functions, add types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-export { World } from './world';
 export { applyCommands } from './commands';
-export { defineSystem } from './systems';
+export { World } from './world';
 export { Entity, initStruct, dropStruct } from './storage';
 export { struct } from './struct';
-export { createThreadChannel } from './threads';
+export { CoreSchedule } from './schedule';
 export { memory } from './utils/memory';
 
+export type { Commands, EntityCommands } from './commands';
+export type { EventReader, EventWriter } from './events';
+export type { Query, Mut, Optional, With, Without, Or } from './queries';
+export type { Res, SystemRes } from './resources';
+export type { ExecutorType, ExecutorInstance } from './schedule';
+export type { Table, Entities } from './storage';
+export type { Struct } from './struct';
+export type { System, SystemParameter } from './systems';
+export type { ThreadGroup } from './threads';
+export type { Memory } from './utils/memory';
 export type { WorldBuilder, Plugin, WorldConfig } from './world';
