fbda8ae5c7fdb9edb99a024a644a9ef7da254d87
JaimeGensler <jaimegensler0@gmail.com>
2023-04-02 11:56:20 -0700

    Update types, scaffold SystemOrder

--- src/schedule/order.ts
+++ src/schedule/order.ts
@@ -1,19 +1,49 @@
-import { SystemDefinition } from '../systems';
+import type { System } from '../systems';
 
 class SystemOrder {
-	before(...systems: SystemDefinition[]): this {
+	constructor() {}
+
+	/**
+	 * Specifies that this system must run _before_ the provided systems may run.
+	 * @param ...systems The systems to run before this system
+	 * @returns `this`, for chaining.
+	 */
+	before(...systems: System[]): this {
 		return this;
 	}
-	after(...systems: SystemDefinition[]): this {
+
+	/**
+	 * Specifies that this system must run _after_ the provided systems have run.
+	 * @param ...systems The systems to run before this system
+	 * @returns `this`, for chaining.
+	 */
+	after(...systems: System[]): this {
+		return this;
+	}
+
+	/**
+	 * Specifies that this system should try to run before any other systems in the schedule have run.
+	 * Systems ordered to run before this will still run before.
+	 * @returns `this`, for chaining.
+	 */
+	first(): this {
+		return this;
+	}
+
+	/**
+	 * Specifies that this system should try to run after all other systems in the schedule have run.
+	 * @returns `this`, for chaining.
+	 */
+	last(): this {
 		return this;
 	}
 }
 
-function order(...systems: SystemDefinition[]): SystemOrder {
+function order(...systems: System[]): SystemOrder {
 	return new SystemOrder();
 }
 
-function chain(...systems: SystemDefinition[]) {}
+function chain(...systems: System[]) {}
 order.chain = chain;
 
 export { order };
