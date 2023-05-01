a301255e316a4adf98b68dd3c51b8a2bbbef7cfa
JaimeGensler <jaimegensler0@gmail.com>
2023-04-30 21:11:09 -0700

    Outline of fixed update

--- src/world/defaultPlugin.ts
+++ src/world/defaultPlugin.ts
@@ -7,26 +7,34 @@ import type { World } from './World';
 
 export type Plugin = (worldBuilder: WorldBuilder) => void;
 
-// thyseus-ignore
-async function runOuterSchedule(world: World) {
+async function runInnerSchedules(world: World) {
 	if (world.schedules[CoreSchedule.Outer].length > 1) {
-		// Adding systems to outer schedules means the consumer has opted out of scheduling!
+		// Adding systems to outer schedules means the consumer has opted out of default outer scheduling
 		return;
 	}
 
 	await world.runSchedule(CoreSchedule.Startup);
-	async function loop(timestep: number) {
+	let previousTime = 0;
+	let delta = 0;
+
+	async function loop(currentTime: number) {
 		await world.runSchedule(CoreSchedule.Main);
+		delta = currentTime - previousTime;
+		for (let i = delta; i > 20; i -= 20) {
+			// TODO: Test
+			await world.runSchedule(CoreSchedule.FixedUpdate);
+		}
+
 		requestAnimationFrame(loop);
 	}
 	loop(0);
 }
-runOuterSchedule.parameters = [new WorldDescriptor()];
+runInnerSchedules.parameters = [new WorldDescriptor()];
 
 export function defaultPlugin(builder: WorldBuilder) {
 	builder
 		.registerComponent(Entity)
+		.addSystemsToSchedule(CoreSchedule.Outer, runInnerSchedules)
 		.addSystemsToSchedule(CoreSchedule.Main, run(applyCommands).last())
-		.addSystemsToSchedule(CoreSchedule.Startup, run(applyCommands).last())
-		.addSystemsToSchedule(CoreSchedule.Outer, runOuterSchedule);
+		.addSystemsToSchedule(CoreSchedule.Startup, run(applyCommands).last());
 }
