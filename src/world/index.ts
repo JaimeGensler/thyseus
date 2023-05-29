0f06565c371d6fa7a829b28de9ad1e361dddf3a9
JaimeGensler <jaimegensler0@gmail.com>
2023-05-29 16:54:48 -0700

    Fix import

--- src/world/index.ts
+++ src/world/index.ts
@@ -1,6 +1,6 @@
 export { World } from './World';
 export { WorldDescriptor } from './WorldDescriptor';
 
-export type { Plugin } from './defaultPlugin';
+export type { Plugin } from './Plugin';
 export type { WorldBuilder } from './WorldBuilder';
 export type { WorldConfig } from './config';
