f5874ba32ee8a0d564e44b57472dd4cda7ac06d1
JaimeGensler <jaimegensler0@gmail.com>
2023-06-17 11:56:04 -0700

    Rewrite commands to use structs

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,10 @@
 export { Commands } from './Commands';
 export { CommandsDescriptor } from './CommandsDescriptor';
 export { applyCommands } from './applyCommands';
+export {
+	AddComponentCommand,
+	RemoveComponentCommand,
+	ClearEventQueueCommand,
+} from './commandTypes';
 
 export type { EntityCommands } from './EntityCommands';
