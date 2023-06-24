086e1bbab4ac0209266b76cb64963d5034b35462
JaimeGensler <jaimegensler0@gmail.com>
2023-06-24 11:51:50 -0700

    Remove implicit dependencies

--- src/schedule/executors/getSystemDependencies.ts
+++ src/schedule/executors/getSystemDependencies.ts
@@ -56,36 +56,6 @@ export function getSystemDependencies(
 		);
 	}
 
-	for (let i = 0; i < systems.length; i++) {
-		const system = systems[i];
-		if (typeof system === 'function') {
-			continue;
-		}
-		if (system.isFirst) {
-			// beforeAll
-			for (let bit = 0; bit < systems.length; bit++) {
-				if (
-					bit !== i &&
-					(deepDependencies[i] & (1n << BigInt(bit))) === 0n
-				) {
-					masks[bit] |= 1n << BigInt(i);
-					deepDependencies[bit] |= 1n << BigInt(i);
-				}
-			}
-		} else if (system.isLast) {
-			// afterAll
-			for (let bit = 0; bit < systems.length; bit++) {
-				if (
-					bit !== i &&
-					(deepDependencies[bit] & (1n << BigInt(i))) === 0n
-				) {
-					masks[i] |= 1n << BigInt(bit);
-					deepDependencies[i] |= 1n << BigInt(bit);
-				}
-			}
-		}
-	}
-
 	return masks;
 }
 
@@ -119,64 +89,6 @@ if (import.meta.vitest) {
 					run(d).before(a),
 				]),
 			).toStrictEqual([0b1000n, 0b0101n, 0b0001n, 0b0000n]);
-		});
-
-		it('creates dependencies with beforeAll/afterAll', () => {
-			const [a, b, c, d] = createMockSystems(4);
-			expect(
-				getSystemDependencies([run(a).last(), b, c, run(d).first()]),
-			).toStrictEqual([0b1110n, 0b1000n, 0b1000n, 0b0000n]);
-		});
-
-		it('prioritizes explicit dependencies over implicit', () => {
-			const [a1, b1, c1] = createMockSystems(3);
-
-			expect(
-				getSystemDependencies([run(a1).last(), run(b1).after(a1), c1]),
-			).toStrictEqual([0b100n, 0b001n, 0b000n]);
-
-			const [a2, b2, c2] = createMockSystems(3);
-			expect(
-				getSystemDependencies([
-					a2,
-					run(b2).before(c2),
-					run(c2).first(),
-				]),
-			).toStrictEqual([0b100n, 0b000n, 0b010n]);
-		});
-
-		it('evaluates in passed order when identical implicit dependencies are provided', () => {
-			const [a1, b1, c1, d1] = createMockSystems(4);
-			expect(
-				getSystemDependencies([
-					run(a1).first(),
-					run(b1).first(),
-					c1,
-					run(d1).first(),
-				]),
-			).toStrictEqual([0b0000n, 0b0001n, 0b1011n, 0b0011n]);
-
-			const [a2, b2, c2, d2] = createMockSystems(4);
-			expect(
-				getSystemDependencies([
-					a2,
-					run(b2).last(),
-					run(c2).last(),
-					run(d2).last(),
-				]),
-			).toStrictEqual([0b0000n, 0b1101n, 0b1001n, 0b0001n]);
-		});
-
-		// Changelog v0.8 bugfix
-		it('does not create circular dependencies', () => {
-			const [a, b, c] = createMockSystems(3);
-			expect(
-				getSystemDependencies([
-					run(a).before(b),
-					run(b).before(c),
-					run(c).first(),
-				]),
-			).toStrictEqual([0b000n, 0b001n, 0b010n]);
 		});
 
 		it('throws for directly contradictory dependencies', () => {
