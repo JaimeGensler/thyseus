1dc67ab3adc7dde99bbaf13f3a01bbe7a7b9556b
JaimeGensler <jaimegensler0@gmail.com>
2023-07-01 17:26:50 -0500

    Fix serialize, update Entity

--- src/storage/Entity.ts
+++ src/storage/Entity.ts
@@ -1,43 +1,60 @@
 import { Memory } from '../utils';
+import type { u32, u64 } from '../struct';
 
+/**
+ * A component that can be used to get the id, index, and generation of an Entity.
+ * All living entities have the `Entity` component.
+ *
+ * Should always be accessed readonly.
+ */
 export class Entity {
-	static size = 8;
+	// Despite Entity consisting of two u32s, we set the alignment to 8 so that
+	// Entity is still guaranteed to be component id 0 even after alignment sort
 	static alignment = 8;
+	static size = 8;
 
-	private declare __$$b: number;
-
-	#id: bigint = 0n;
-	#index: number = 0;
-	#generation: number = 0;
+	private __$$b: number = 0;
+	#index: u32 = 0;
+	#generation: u32 = 0;
 
 	deserialize() {
-		this.#id = Memory.u64[this.__$$b >> 3];
 		this.#index = Memory.u32![this.__$$b >> 2];
 		this.#generation = Memory.u32![(this.__$$b + 4) >> 2];
 	}
-	serialize() {
-		// This is a no-op because Entity is immutable.
+
+	// This is a no-op because Entity is immutable.
+	serialize() {}
+
+	/**
+	 * Determines if this entity is the same as another entity.
+	 * @param other The entity to compare against.
+	 * @returns A boolean indicating if this entity refers to the provided entity.
+	 */
+	is(other: Readonly<Entity>): boolean {
+		return (
+			this.index === other.index && this.generation === other.generation
+		);
 	}
 
 	/**
-	 * The entity's world-unique integer id (uint64).
+	 * The entity's world-unique integer id (u64).
 	 * Composed of an entity's generation & index.
 	 */
-	get id(): bigint {
-		return this.#id;
+	get id(): u64 {
+		return (BigInt(this.#generation) << 32n) | BigInt(this.#index);
 	}
 
 	/**
-	 * The index of this entity (uint32).
+	 * The index of this entity (u32).
 	 */
-	get index(): number {
+	get index(): u32 {
 		return this.#index;
 	}
 
 	/**
-	 * The generation of this entity (uint32).
+	 * The generation of this entity (u32).
 	 */
-	get generation(): number {
+	get generation(): u32 {
 		return this.#generation;
 	}
 }
