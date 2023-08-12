c66818912844a41823c5c0f817f7d5a5e12bd719
JaimeGensler <jaimegensler0@gmail.com>
2023-08-12 09:13:25 -0700

    Reorganize for monorepo

---
+++ packages/thyseus/README.md
@@ -0,0 +1,11 @@
+# Thyseus
+
+The core package for [Thyseus](https://thyseus.dev).
+
+Visit [the docs](https://thyseus.dev/docs) for usage.
+
+## Installation
+
+```sh
+pnpm add thyseus
+```
--- packages/thyseus/src/storage/index.ts
+++ packages/thyseus/src/storage/index.ts
@@ -1,4 +1,4 @@
 export { Table } from './Table';
-export { Entity } from './Entity';
+export { Entity, Entity2 } from './Entity';
 export { Entities } from './Entities';
 export { Vec } from './Vec';
