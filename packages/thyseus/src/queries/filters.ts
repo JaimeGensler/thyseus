028cdf0f34f4c62c87327dad1650b480a046271d
JaimeGensler <jaimegensler0@gmail.com>
2023-09-04 11:25:43 -0700

    Fix tests

--- packages/thyseus/src/queries/filters.ts
+++ packages/thyseus/src/queries/filters.ts
@@ -80,12 +80,13 @@ export function createArchetypeFilter(
 		);
 	}
 }
+export function DEV_ASSERT_FILTER_VALID(filters: bigint[]) {}
 
 /*---------*\
 |   TESTS   |
 \*---------*/
 if (import.meta.vitest) {
-	const { it, expect } = import.meta.vitest;
+	const { describe, it, expect } = import.meta.vitest;
 
 	class Comp {
 		static size = 0;
@@ -96,97 +97,99 @@ if (import.meta.vitest) {
 	class C extends Comp {}
 	class D extends Comp {}
 	class E extends Comp {}
-	const components = [A, B, C, D, E];
+	const components: Struct[] = [A, B, C, D, E];
 
-	const getArchetype = (...components: Struct[]) =>
-		components.reduce(
+	const getArchetype = (...comps: Struct[]) =>
+		comps.reduce(
 			(acc, val) => acc | (1n << BigInt(components.indexOf(val))),
 			0n,
 		);
 	const createPlainFilter = (filter: Filter) =>
 		createArchetypeFilter(filter, [0n, 0n], getArchetype);
 
-	it('works with simple With filters', () => {
-		for (let i = 0; i < components.length; i++) {
-			expect(createPlainFilter(new With(components[i]))).toStrictEqual([
-				1n << BigInt(i),
-				0n,
+	describe('createArchetypeFilter()', () => {
+		it('works with simple With filters', () => {
+			for (let i = 0; i < components.length; i++) {
+				expect(
+					createPlainFilter(new With(components[i])),
+				).toStrictEqual([1n << BigInt(i), 0n]);
+			}
+		});
+
+		it('works with simple Without filters', () => {
+			for (let i = 0; i < components.length; i++) {
+				expect(
+					createPlainFilter(new Without(components[i])),
+				).toStrictEqual([0n, 1n << BigInt(i)]);
+			}
+		});
+
+		it('works with And filters (tuples)', () => {
+			expect(
+				createPlainFilter(
+					new And(new With(A, B, D), new Without(C, E)),
+				),
+			).toStrictEqual([0b01011n, 0b10100n]);
+		});
+
+		it('works with simple Or filters', () => {
+			expect(
+				createPlainFilter(new Or(new With(A), new With(B))),
+			).toStrictEqual([0b00001n, 0n, 0b00010n, 0n]);
+
+			expect(
+				createPlainFilter(new Or(new With(E), new Without(C))),
+			).toStrictEqual([0b10000n, 0n, 0b0n, 0b00100n]);
+		});
+
+		it('works with complex Or filters', () => {
+			expect(
+				createPlainFilter(
+					// A && !B && (D || E)
+					new And(
+						new With(A),
+						new Or(new With(D), new With(E)),
+						new Without(B),
+					),
+				),
+			).toStrictEqual([0b01001n, 0b00010n, 0b10001n, 0b00010n]);
+
+			expect(
+				createPlainFilter(
+					// A || (B || C)
+					new Or(new With(A), new Or(new With(B), new With(C))),
+				),
+			).toStrictEqual([0b001n, 0n, 0b010n, 0n, 0b100n, 0n]);
+
+			expect(
+				createPlainFilter(
+					// (A || B) && (!C || !D)
+					new And(
+						new Or(new With(A), new With(B)),
+						new Or(new Without(C), new Without(D)),
+					),
+				),
+			).toStrictEqual([
+				0b0001n,
+				0b0100n,
+				0b0010n,
+				0b0100n,
+				0b0001n,
+				0b1000n,
+				0b0010n,
+				0b1000n,
 			]);
-		}
-	});
+		});
 
-	it('works with simple Without filters', () => {
-		for (let i = 0; i < components.length; i++) {
-			expect(createPlainFilter(new Without(components[i]))).toStrictEqual(
-				[0n, 1n << BigInt(i)],
-			);
-		}
-	});
-
-	it('works with And filters (tuples)', () => {
-		expect(
-			createPlainFilter(new And(new With(A, B, D), new Without(C, E))),
-		).toStrictEqual([0b01011n, 0b10100n]);
-	});
-
-	it('works with simple Or filters', () => {
-		expect(
-			createPlainFilter(new Or(new With(A), new With(B))),
-		).toStrictEqual([0b00001n, 0n, 0b00010n, 0n]);
-
-		expect(
-			createPlainFilter(new Or(new With(E), new Without(C))),
-		).toStrictEqual([0b10000n, 0n, 0b0n, 0b00100n]);
-	});
-
-	it('works with complex Or filters', () => {
-		expect(
-			createPlainFilter(
-				// A && !B && (D || E)
-				new And(
-					new With(A),
-					new Or(new With(D), new With(E)),
-					new Without(B),
+		it('works with initial values', () => {
+			expect(
+				createArchetypeFilter(
+					new And(new With(C), new Without(D)),
+					[0b0001n, 0b0010n],
+					getArchetype,
 				),
-			),
-		).toStrictEqual([0b01001n, 0b00010n, 0b10001n, 0b00010n]);
-
-		expect(
-			createPlainFilter(
-				// A || (B || C)
-				new Or(new With(A), new Or(new With(B), new With(C))),
-			),
-		).toStrictEqual([0b001n, 0n, 0b010n, 0n, 0b100n, 0n]);
-
-		expect(
-			createPlainFilter(
-				// (A || B) && (!C || !D)
-				new And(
-					new Or(new With(A), new With(B)),
-					new Or(new Without(C), new Without(D)),
-				),
-			),
-		).toStrictEqual([
-			0b0001n,
-			0b0100n,
-			0b0010n,
-			0b0100n,
-			0b0001n,
-			0b1000n,
-			0b0010n,
-			0b1000n,
-		]);
-	});
-
-	it.todo('works with initial values', () => {
-		expect(
-			createFilterBitfields(
-				components,
-				[A, B],
-				[],
-				[new Without(C), new With(D)],
-			),
-		).toStrictEqual([0b1011n, 0b0100n]);
+			).toStrictEqual([0b0101n, 0b1010n]);
+		});
 	});
 
 	it.todo('throws if filters are impossible', () => {
