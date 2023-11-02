1f8bca6b324a09ff7358b78b444534f6c8e399b3
JaimeGensler <jaimegensler0@gmail.com>
2023-11-02 09:19:02 -0700

    Simplify, document

--- packages/thyseus/src/utils/swapRemove.ts
+++ packages/thyseus/src/utils/swapRemove.ts
@@ -1,7 +1,12 @@
+/**
+ * Given an array, swaps the element at the provided index with the last element, and then pops the now-last element off the array.
+ * @param array The array to modify.
+ * @param index The index to swap and remove.
+ * @returns The element that was removed.
+ */
 export function swapRemove<T>(array: T[], index: number): T | undefined {
-	const lastIndex = array.length - 1;
 	const temp = array[index];
-	array[index] = array[lastIndex];
-	array[lastIndex] = temp;
-	return array.pop();
+	array[index] = array[array.length - 1];
+	array.pop();
+	return temp;
 }
