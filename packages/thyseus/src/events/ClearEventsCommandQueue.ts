beed90cb3924e9198c70da3557e8121248a60a99
JaimeGensler <jaimegensler0@gmail.com>
2023-11-02 09:19:55 -0700

    Simplify

--- packages/thyseus/src/events/ClearEventsCommandQueue.ts
+++ packages/thyseus/src/events/ClearEventsCommandQueue.ts
@@ -1,19 +1,20 @@
 import type { World } from '../world';
 
 import { Events } from './Events';
+import type { EventWriter } from './EventQueues';
 
 export class ClearEventsCommandQueue {
-	#events: Events;
+	#writers: EventWriter<any>[];
 	eventQueuesToClear: number[];
 
 	constructor(world: World) {
-		this.#events = world.getResource(Events)!;
+		this.#writers = world.getResource(Events)!.writers;
 		this.eventQueuesToClear = [];
 	}
 
 	apply(world: World) {
 		for (const queueId of this.eventQueuesToClear) {
-			this.#events.writers[queueId].clearImmediate();
+			this.#writers[queueId].clearImmediate();
 		}
 		this.eventQueuesToClear.length = 0;
 	}
