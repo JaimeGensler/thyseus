804f2d7141c65e408852ae769aebb3937551f4d8
JaimeGensler <jaimegensler0@gmail.com>
2023-12-16 10:48:00 -0800

    Return promise

--- packages/thyseus/src/world/World.ts
+++ packages/thyseus/src/world/World.ts
@@ -140,7 +140,7 @@ export class World {
 				scheduleType.name,
 			)}" in the world!`,
 		);
-		this.schedules.get(scheduleType)!.run();
+		return this.schedules.get(scheduleType)!.run();
 	}
 
 	/**
