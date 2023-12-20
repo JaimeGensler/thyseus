6c4bf019696dcc1a08d88b62392ae75d0640b81b
JaimeGensler <jaimegensler0@gmail.com>
2023-12-19 21:05:55 -0800

    Permit async plugins

--- packages/thyseus/src/world/Plugin.ts
+++ packages/thyseus/src/world/Plugin.ts
@@ -3,4 +3,6 @@ import { World } from './World';
 /**
  * A function that takes a world and may add event listeners, systems, and data to it.
  */
-export type Plugin = (world: World) => void;
+export type Plugin =
+	| ((world: World) => Promise<void>)
+	| ((world: World) => void);
