3c65ff67167206e2fa1a80727b03a980f96ce7a0
JaimeGensler <jaimegensler0@gmail.com>
2023-12-31 14:56:03 -0800

    Remove ReadModifier, Read

--- packages/thyseus/src/systems/createSystem.ts
+++ packages/thyseus/src/systems/createSystem.ts
@@ -1,52 +1,53 @@
-import { Commands } from '../commands';
-import { EventReader, EventWriter } from '../events';
-import { Query } from '../queries';
-import { Res, SystemRes } from '../resources';
-import { Thread } from '../threads';
-import { World } from '../world';
-import { SystemParameter } from './System';
+export default {};
+// import { Commands } from '../commands';
+// import { EventReader, EventWriter } from '../events';
+// import { Query } from '../queries';
+// import { Res, SystemRes } from '../resources';
+// import { Thread } from '../threads';
+// import { World } from '../world';
+// import { SystemParameter } from './System';
 
-const defaultSystemParameters = {
-	query: Query,
-	res: Res,
-	systemRes: SystemRes,
-	eventReader: EventReader,
-	eventWriter: EventWriter,
-	thread: Thread,
-	world: World,
-	commands: Commands,
-} as const;
-type DefaultSystemParameters = typeof defaultSystemParameters;
+// const defaultSystemParameters = {
+// 	query: Query,
+// 	res: Res,
+// 	systemRes: SystemRes,
+// 	eventReader: EventReader,
+// 	eventWriter: EventWriter,
+// 	thread: Thread,
+// 	world: World,
+// 	commands: Commands,
+// } as const;
+// type DefaultSystemParameters = typeof defaultSystemParameters;
 
-type SecondaryArguments<T> = T extends (world: World, ...rest: infer R) => any
-	? R
-	: never;
-type ReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+// type SecondaryArguments<T> = T extends (world: World, ...rest: infer R) => any
+// 	? R
+// 	: never;
+// type ReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
 
-type SystemCreator<
-	P extends any[],
-	T extends Record<string, SystemParameter>,
-> = {
-	[Key in keyof T]: (
-		...args: SecondaryArguments<T[Key]['intoArgument']>
-	) => SystemCreator<[...P, ReturnType<T[Key]['intoArgument']>], T>;
-} & { build<Z extends (...args: P) => void>(system: Z): Z };
-export function createSystem<T extends Record<string, SystemParameter>>(
-	arg?: T,
-): SystemCreator<[], T & DefaultSystemParameters> {
-	const args = [];
-	const builder = {} as SystemCreator<[], T & DefaultSystemParameters>;
-	for (const key in { ...defaultSystemParameters, ...arg }) {
-		builder[key as keyof typeof builder] = (...args: any[]) => {};
-	}
-	builder.build = (fn: any) => {
-		fn.getSystemArguments = (world: World) => [];
-		return fn;
-	};
-	return builder;
-}
+// type SystemCreator<
+// 	P extends any[],
+// 	T extends Record<string, SystemParameter>,
+// > = {
+// 	[Key in keyof T]: (
+// 		...args: SecondaryArguments<T[Key]['intoArgument']>
+// 	) => SystemCreator<[...P, ReturnType<T[Key]['intoArgument']>], T>;
+// } & { build<Z extends (...args: P) => void>(system: Z): Z };
+// export function createSystem<T extends Record<string, SystemParameter>>(
+// 	arg?: T,
+// ): SystemCreator<[], T & DefaultSystemParameters> {
+// 	const args = [];
+// 	const builder = {} as SystemCreator<[], T & DefaultSystemParameters>;
+// 	for (const key in { ...defaultSystemParameters, ...arg }) {
+// 		builder[key as keyof typeof builder] = (...args: any[]) => {};
+// 	}
+// 	builder.build = (fn: any) => {
+// 		fn.getSystemArguments = (world: World) => [];
+// 		return fn;
+// 	};
+// 	return builder;
+// }
 
-const mySystem = createSystem()
-	.world()
-	.commands()
-	.build((world, cmds) => cmds);
+// const mySystem = createSystem()
+// 	.world()
+// 	.commands()
+// 	.build((world, cmds) => cmds);
