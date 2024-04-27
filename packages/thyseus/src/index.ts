7740714fa8196d3841baa8e8611598927f798e7b
JaimeGensler <jaimegensler0@gmail.com>
2024-04-27 09:44:27 -0700

    Export cleanup

--- packages/thyseus/src/index.ts
+++ packages/thyseus/src/index.ts
@@ -1,15 +1,21 @@
 // Top-level API
 export { Tag } from './components';
 export { applyEntityUpdates, Entity } from './entities';
-export { EventReader, Events, EventWriter } from './events';
+export {
+	EventReader,
+	Events,
+	EventWriter,
+	clearAllEventQueues,
+} from './events';
 export { And, Maybe, Or, Query, With, Without } from './queries';
-export { Res, Local } from './resources';
+export { Local, Res } from './resources';
 export { cloneSystem } from './systems';
 export { expose, Thread, Threads } from './threads';
 export { Schedule, World } from './world';
 
 // Types
-export type { Table, Class } from './components';
+export type { Class, Table, TagComponentType } from './components';
+export type { Entities } from './entities';
 export type { Filter } from './queries';
 export type { System, SystemParameter } from './systems';
 export type { StructuredCloneable } from './threads';
