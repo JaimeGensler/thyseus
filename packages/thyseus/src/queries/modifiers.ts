0777c3fa8ac23706fc42b0ad51929aec4d74099a
JaimeGensler <jaimegensler0@gmail.com>
2024-04-27 09:01:18 -0700

    Cleanup/fix filters/modifiers

--- packages/thyseus/src/queries/modifiers.ts
+++ packages/thyseus/src/queries/modifiers.ts
@@ -10,9 +10,32 @@ export const Maybe = {
 		return { modifier: 'maybe', type };
 	},
 	isMaybe(value: any): value is { modifier: string; type: Class } {
-		return typeof 'value' === 'object' && value.modifier === 'maybe';
+		return typeof value === 'object' && value.modifier === 'maybe';
 	},
 };
 
 export type AccessorDescriptor = Class | { modifier: string; type: Class };
 export type Accessor = Maybe<object>;
+
+/*---------*\
+|   TESTS   |
+\*---------*/
+if (import.meta.vitest) {
+	const { it, expect, describe } = import.meta.vitest;
+
+	class MyComponent {}
+
+	describe('Maybe', () => {
+		it('intoArgument() returns a maybe descriptor', () => {
+			expect(Maybe.intoArgument({} as any, MyComponent)).toStrictEqual({
+				modifier: 'maybe',
+				type: MyComponent,
+			});
+		});
+		it('isMaybe() returns true iff a value is a maybe descriptor', () => {
+			const maybeComp = Maybe.intoArgument({} as any, MyComponent);
+			expect(Maybe.isMaybe(maybeComp)).toBe(true);
+			expect(Maybe.isMaybe(MyComponent)).toBe(false);
+		});
+	});
+}
